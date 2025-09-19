793ff9365e4236af6ab4a69935ffa0946a1a849a
shiful33 <shwapon.joti@gmail.com>
2025-09-19 08:40:48 +0600

    all added finally all up to date

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({country}) => {
-    console.log(country)
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
+
+    const [visited, setVisited] = useState(false);
+    // console.log(country)
+    console.log(handleVisitedCountries);
 
     const handleVisited = () => {
-        console.log('Visit more...')
+        setVisited(!visited);
+        handleVisitedCountries(country);
     }
     
     return (
-        <div className='p-4 m-4 border-2 border-amber-500 rounded-xl'>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        <div className={`md:px-4 m-4 p-4  border-2 border-amber-500 rounded-xl bg-amber-100 ${visited ? 'bg-amber-300' : 'bg-white'}`}>
+            <img className='w-full md:w-60 md:h-30 mb-4' src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common}</h2>
             <h2>Official Name: {country.name.official}</h2>
             <h3>Capital: {country.capital.capital}</h3>
             {/* <h3>Languages: {country.languages.languages}</h3> */}
             <p>Population: {country.population.population}</p>
 
-            <button onClick={handleVisited}>Explore More</button>
+            <button className='mt-4 px-4 py-2 bg-lime-400 border-none rounded cursor-pointer' onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
+
+            <button className='mt-4 px-4 py-2 bg-lime-400 border-none rounded cursor-pointer ml-2' onClick={() => { handleVisitedFlag(country.flags.flags.png) }}>Add flag</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,11 +1,23 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 
-const Countries = ({countriesPromise}) => {
-    /* const countriesData = use(countriesPromise);
-    const countries = countriesData.countries;
-    console.log(countries) */
 
+const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        // console.log('handle visited', country)
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlag = (flag) => {
+        // console.log('flag need to be added', flag)
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
 
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
@@ -13,12 +25,29 @@ const Countries = ({countriesPromise}) => {
 
     return (
         <>
-        <div className='flex justify-center mb-20'>
-            <h1 className='text-4xl font-semibold'>In World All Countries: {countries.length}</h1>
+        <div className='flex justify-center mb-4'>
+            <h1 className='text-2xl md:text-4xl font-bold'>In World All Countries: {countries.length}</h1>
         </div>
-        <div className='grid grid-cols-4'>
+        <div className='flex justify-center mb-4'>
+            <h2 className='text-2xl font-semibold'>Total Country Visited: {visitedCountries.length}</h2>
+        </div>
+        <div className='flex justify-center mb-4'>
+            <ol>
+                {
+                visitedCountries.map(country => <li>{country.name.common}</li>)
+            }
+            </ol>
+        </div>
+        <div className='flex justify-center mb-20 items-center'>
+            <h2 className='text-2xl font-semibold'>Total Visited Flags: {visitedFlags.length}</h2>
+
             {
-                countries.map(country => <Country country={country}></Country>)
+                visitedFlags.map((flag, index) => <img key={index} src={flag} className='w-14 h-8 ml-4'></img>)
+            }
+        </div>
+        <div className='md:grid grid-cols-4'>
+            {
+                countries.map(country => <Country country={country} handleVisitedCountries={handleVisitedCountries} handleVisitedFlag={handleVisitedFlag}></Country>)
             }
         </div>
         </>
